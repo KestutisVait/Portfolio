a991dfccc226d76a08aa43ff775a020930dc6d1d
KestutisVaitiekunas <kestutis.vaitiekunas@stud.ktmc.lt>
2024-05-28 09:56:12 +0300

    clean up

--- src/components/project_info.jsx
+++ src/components/project_info.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import '../App.css';
 import styled, { keyframes } from 'styled-components';
 // import Gallery from './gallery';
